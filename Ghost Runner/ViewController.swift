5175e171eaaa0f16d75cb68cf54c94f6a68ffded
kamiabay <kamiab.ayani@gmail.com>
2021-02-11 11:06:49 -1000

    initial

---
+++ Ghost Runner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ghost Runner
+//
+//  Created by Kamyab Ayanifard on 2/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
