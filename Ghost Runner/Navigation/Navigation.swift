7abf71b6c4a12b31aeca2bda78d62690dd811b83
kamiabay <kamiab.ayani@gmail.com>
2021-02-18 10:56:46 -1000

    implementing navigation class

---
+++ Ghost Runner/Navigation/Navigation.swift
@@ -0,0 +1,13 @@
+//
+//  Navigation.swift
+//  Ghost Runner
+//
+//  Created by Kamyab Ayanifard on 2/18/21.
+//
+
+import Foundation
+
+// takes care of navigation to other screens 
+class Navigator {
+    
+}
