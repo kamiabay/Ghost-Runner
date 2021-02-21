2c7bf38b7fec8ee595bc8b0f1845f1df5a72179c
zwucd166 <72957266+zwucd166@users.noreply.github.com>
2021-02-20 19:53:02 -0500

    Add files via upload

--- Ghost Runner/controller/LoginVC.swift
+++ Ghost Runner/controller/LoginVC.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class LoginVC: UIViewController {
     
-    @IBOutlet weak var emailField: UITextField!
-    @IBOutlet weak var passwordField: UITextField!
+//    @IButlet weak var emailField: UITextField!
+//    @IBOutlet weak var passwordField: UITextField!
 
     @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var signUpButton: UIButton!
+//    @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var appleLoginButton: UIButton!
     @IBOutlet weak var googleLoginButton: UIButton!
     @IBOutlet weak var facebookLoginButton: UIButton!
@@ -22,24 +22,24 @@ class LoginVC: UIViewController {
     var navigation: Navigator?
     
     var tester = Tester()
-    
-    let gradientLayer = CAGradientLayer()
 
+    let gradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigation = Navigator(currentViewController: self)
         navigation?.currentViewController?.navigationController?.navigationBar.isHidden = true
         
-        emailField.tintColor = UIColor.white
-        emailField.setIcon(#imageLiteral(resourceName: "email_icon"))
-        passwordField.tintColor = UIColor.white
-        passwordField.setIcon(#imageLiteral(resourceName: "pw_icon"))
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tapGesture)
+        
+//        emailField.setIcon(#imageLiteral(resourceName: "email_icon"))
+//        passwordField.setIcon(#imageLiteral(resourceName: "pw_icon"))
         
         loginButton.layer.cornerRadius = 20.0
         appleLoginButton.layer.cornerRadius = 20.0
         googleLoginButton.layer.cornerRadius = 20.0
         facebookLoginButton.layer.cornerRadius = 20.0
-
 
         gradientLayer.colors = [UIColor(#colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)).cgColor, UIColor(#colorLiteral(red: 0, green: 0.749853909, blue: 0.7112129927, alpha: 1)).cgColor]
         gradientLayer.locations = [0.0, 1.0]
@@ -47,18 +47,23 @@ class LoginVC: UIViewController {
         self.view.layer.insertSublayer(gradientLayer, at:0)
     }
 
-    @IBAction func signup() {
+    
+    @IBAction func loginWithEmail() {
+        // TODO: SAVE USER IN LOCAL STORAGE
         navigation?.goToSignUp()
     }
     
-    @IBAction func login() {
-        // TODO: SAVE USER IN LOCAL STORAGE
-        navigation?.goToHome()
+    @IBAction func loginWithApple() {
     }
     
+    @IBAction func loginWithGoogle() {
+    }
     
+    @IBAction func loginWithFacebook() {
+    }
 }
 
+// MARK: - Text Field extension to add images
 extension UITextField {
     func setIcon(_ image: UIImage) {
        let iconView = UIImageView(frame:
