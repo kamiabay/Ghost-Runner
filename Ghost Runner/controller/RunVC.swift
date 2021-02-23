10670082de954d9e1666dd01a55a16acb6a5091e
Nikita Andrikanis <nandrikanis@ucdavis.edu>
2021-02-22 21:35:02 -0800

    Implemented User and Ghost functionality for prototype 1, let's goooo

--- Ghost Runner/controller/RunVC.swift
+++ Ghost Runner/controller/RunVC.swift
@@ -14,31 +14,36 @@ import MapKit
 class RunVC: UIViewController {
     var opponentRun: Run?; // NEEDS TO BE INITIALIZED
     let db = DB()
+    var runSnapshotList = [RunSnapshot]();
+    
+    
+    // Timers
     var runTimer: Timer?
     var userTrackTimer: Timer?
     var opponenetTimer: Timer?
-    
-    let locationManager = CLLocationManager();
-    var currentLoc: CLLocation?
-    var runCalculation: RunCalculation?;
-    
+  
+    // Storyboard Outlets
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var mapView: MKMapView!
     
-
+    // CoreLocation Location Manager instance
+    let locationManager = CLLocationManager();
+    
+    // Ghost location
+    dynamic var ghostDot = MKPointAnnotation()
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor =  .systemOrange
-        mapView.delegate = self
-        setupLocationManagment()
-        runCalculation = RunCalculation.init(opponentRun: opponentRun!);
         
-
+        // General view styling
+        view.backgroundColor =  .darkGray
+        
+        // Set up observer in background
         NotificationCenter.default.addObserver(self, selector: #selector(resumeObservingUser), name: UIApplication.willEnterForegroundNotification, object: nil)
         
-        // Beging to update the user's location on the app after location is authorized
+        // In case user did not enable location, this will ask again, stalling our user/ghost functions
         let authorizeLocQueue = DispatchQueue(label: "authorizelocation")
         authorizeLocQueue.async {
             if !(CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() == .authorizedAlways) {
@@ -47,27 +52,84 @@ class RunVC: UIViewController {
             }
         }
         
+        // Wait until there is location approval to begin user/ghost functions
         authorizeLocQueue.async {
             while !(CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() == .authorizedAlways) {}
-            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            self.locationManager.distanceFilter = kCLDistanceFilterNone
-            
-            self.locationManager.startUpdatingLocation()
-            self.mapView.showsUserLocation = true
-            self.setInitialZoom()
-            self.startObservingUser()
-            print("began observing user")
+            self.CLandMapKitSetup()
+            self.beginUpdatingMap()
         }
-        
     }
     
+    // Deinit notification for when app enters foreground
     deinit {
         NotificationCenter.default.removeObserver(self, name: UIApplication.willEnterForegroundNotification, object: nil)
     }
     
-    @objc func resumeObservingUser() {
-        self.startObservingUser()
+    
+    // #######################################
+    // CL/MK Set-up
+    // #######################################
+    
+    func CLandMapKitSetup() {
+        // For Kami's background support
+        locationManager.delegate = self
+        locationManager.allowsBackgroundLocationUpdates = true
+        
+        if #available(iOS 14.0, *) {
+            locationManager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "to update best") { (Error) in
+                print("done")
+            }
+        } else {
+            print("locationManager needs to fall back onto older version")
+        }
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
     }
+    
+    func beginUpdatingMap() {
+        self.locationManager.startUpdatingLocation()
+        //self.locationManager.startUpdatingHeading()
+        self.mapView.showsUserLocation = true
+        self.setInitialZoom()
+        self.startObservingUser()
+        print("Began observing user")
+        
+        self.mapView.addAnnotation(self.ghostDot)
+        self.animateOpponent()
+        print("Began animating opponent")
+    }
+    
+    // #######################################
+    // Ghost/Opponent Functions
+    // #######################################
+    
+    @IBAction func animateOpponent() {
+        beginGhostAnimation();
+    }
+    
+    func beginGhostAnimation() {
+        runTimer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateOpponentLocation), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateOpponentLocation()  {
+        //print("\(opponentRun?.getNextRunLocation().toJSON())")
+        //mapView.setCenter((opponentRun?.getNextRunLocation().get2DCordinate())!, animated: true)
+        
+        //let mkAnnotation: MKPointAnnotation = MKPointAnnotation()
+        //mkAnnotation.coordinate = (opponentRun?.getNextRunLocation().get2DCordinate())!
+        
+        if let nextCoord = opponentRun?.getNextRunLocation().get2DCordinate() {
+            UIView.animate(withDuration: 2) {
+                self.ghostDot.coordinate.latitude = nextCoord.latitude
+                self.ghostDot.coordinate.longitude = nextCoord.longitude
+            }
+        }
+    }
+    
+    // #######################################
+    // User GPS Tracking/Animation
+    // #######################################
     
     func setInitialZoom() {
         if let currLoc = locationManager.location?.coordinate {
@@ -83,82 +145,44 @@ class RunVC: UIViewController {
         }
     }
     
+    @objc func resumeObservingUser() {
+        self.startObservingUser()
+    }
+    
     @objc func updateMapCenter() {
+        if let currLoc = locationManager.location?.coordinate {
+            mapView.setCenter(currLoc, animated: true)
+            print("updating center!")
+        }
+    }
+    
+    
+    // #######################################
+    // User Run/Snapshot Recording
+    // #######################################
+    
+    @objc func startCollectingGPSData()  {
         if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() == .authorizedAlways) {
-            if let currLoc = locationManager.location?.coordinate {
-                mapView.setCenter(currLoc, animated: true)
-            }
-        }
-    }
-
-    
-    
-    
-    @objc
-    func updateOpponentLocation()  {
-        let location = opponentRun!.getNextRunLocation().get2DCordinate()
-        mapView.setCenter(location, animated: true)
-        let mkAnnotation: MKPointAnnotation = MKPointAnnotation()
-        mkAnnotation.coordinate = location
-        mapView.addAnnotation(mkAnnotation)
-       
-    }
-    
-    // TESTING : REMOVE LATER
-    func startAnimateOpponenrRun() {
-        runTimer = Timer.scheduledTimer(timeInterval: 0.025, target: self, selector: #selector(updateOpponentLocation), userInfo: nil, repeats: true)
-    }
-    
-    @objc
-    func startCollectingGPSData()  {
-        if(CLLocationManager.authorizationStatus() == .authorizedAlways) {
+            print("saving GPS")
             let gps = GPS(locationManager: locationManager);
-            
-            runCalculation!.updateOwnRunAndGetOpponentLocation(runSnapshot: RunSnapshot(gps: gps))
+            runSnapshotList.append(RunSnapshot(gps: gps));
+            print("\(runSnapshotList.count): saving gps : \(gps)")
         }
-    }
-    
-
-    func setupLocationManagment() {
-        locationManager.delegate = self
-        locationManager.allowsBackgroundLocationUpdates = true
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        if #available(iOS 14.0, *) {
-            locationManager.requestTemporaryFullAccuracyAuthorization(withPurposeKey: "to update best") { (Error) in
-                print("done")
-            }
-        } else {
-            // Fallback on earlier versions
-        };
-        
     }
     
     func saveRunData()  {
-        let ownRunList = runCalculation!.getOwnFinalRunList();
-        print(ownRunList);
-        if (!ownRunList.isEmpty) {
+        if (!runSnapshotList.isEmpty) {
             runTimer?.invalidate() // end the timer
-            db.runDb.saveRunSnapShot(runSnapShotList: ownRunList);
+            db.runDb.saveRunSnapShot(runSnapShotList: runSnapshotList);
         }
       
     }
-    
-    func showPath()  {
-        let routeLine = runCalculation!.getOpponentFullMKPolyline()
-        self.mapView.addOverlay(routeLine)
-    }
-    
-    @IBAction func animateOpponent() {
-        showPath()
-        startAnimateOpponenrRun();
-    }
-    
     
     @IBAction func startPress(_ sender: UIButton) {
         view.backgroundColor =  .systemGreen
         
         locationManager.showsBackgroundLocationIndicator = true
-        runCalculation!.deleteOwnRunList()
+        runSnapshotList.removeAll()
         
         // enable listner for background GPS change
         locationManager.startUpdatingLocation()
@@ -181,19 +205,8 @@ class RunVC: UIViewController {
         locationManager.stopUpdatingHeading();
         locationManager.stopUpdatingLocation();
     }
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 extension RunVC: CLLocationManagerDelegate {
@@ -201,25 +214,28 @@ extension RunVC: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateHeading heading: CLHeading) {
         //print(heading.magneticHeading)
     }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
           //  print("New location is \(location)")
         }
     }
-    
-    
 }
 
-extension RunVC: MKMapViewDelegate {
-    
-    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-       
-        let polylineRenderer = MKPolylineRenderer(overlay: overlay)
-        polylineRenderer.strokeColor = .systemRed
-        polylineRenderer.lineWidth = 5
-        return polylineRenderer
-      
+
+func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    guard annotation is MKPointAnnotation else { return nil }
+
+    let identifier = "Annotation"
+    var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+    if annotationView == nil {
+        annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        annotationView!.canShowCallout = true
+    } else {
+        annotationView!.annotation = annotation
     }
-    
+
+    return annotationView
 }
 
