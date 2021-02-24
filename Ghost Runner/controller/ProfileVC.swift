a3e1cfba74544ccd2d55f7de015386a1e6912d87
dylanalong <dylanalong@gmail.com>
2021-02-23 21:37:26 -0800

    profile picture funcionality added

--- Ghost Runner/controller/ProfileVC.swift
+++ Ghost Runner/controller/ProfileVC.swift
@@ -8,9 +8,15 @@
 import Foundation
 import UIKit
 
-class ProfileVC: UIViewController {
+class ProfileVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     var navigation: Navigator?
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet var tapGestureRecognizer: UITapGestureRecognizer!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userID: UIButton!
+    
+    let localStorage = LocalStorage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,12 +25,36 @@ class ProfileVC: UIViewController {
         
         navigation = Navigator(currentViewController: self)
         
+        profileImage.layer.cornerRadius = 70
+        profileImage.clipsToBounds = true
+        
+        profileImage.addGestureRecognizer(tapGestureRecognizer)
+        
+        let userInfo = localStorage.getUser()
+        
+        self.userName.text = "Username: \(userInfo.name)"
+        self.userID.setTitle("User ID: \(userInfo.uid)", for: .normal)
     }
 
+    @IBAction func tapProfileImage(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            profileImage.image = image
+            
+           // let userDb = UserDb()
+            
+            
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func homeButtonPress(_ sender: UIButton) {
         navigation?.goToHome()
     }
-    
-
 }
 
