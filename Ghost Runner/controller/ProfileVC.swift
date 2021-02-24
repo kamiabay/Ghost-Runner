70e9e70c4bfb9e255e84922fbee280d9f5a96f34
dylanalong <dylanalong@gmail.com>
2021-02-23 23:32:12 -0800

    can click user id to send text with id

--- Ghost Runner/controller/ProfileVC.swift
+++ Ghost Runner/controller/ProfileVC.swift
@@ -7,8 +7,9 @@
 
 import Foundation
 import UIKit
+import MessageUI
 
-class ProfileVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ProfileVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MFMessageComposeViewControllerDelegate {
 
     var navigation: Navigator?
     @IBOutlet weak var profileImage: UIImageView!
@@ -17,10 +18,13 @@ class ProfileVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     @IBOutlet weak var userID: UIButton!
     
     let localStorage = LocalStorage()
+    var user = ""
+    var uid = ""
+    var image: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor =  .systemOrange
+        view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
         
         navigation = Navigator(currentViewController: self)
@@ -32,8 +36,10 @@ class ProfileVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         
         let userInfo = localStorage.getUser()
         
-        self.userName.text = "Username: \(userInfo.name)"
-        self.userID.setTitle("User ID: \(userInfo.uid)", for: .normal)
+        self.user = userInfo.name
+        self.uid = userInfo.uid
+        self.userName.text = "Username: \(user)"
+        self.userID.setTitle("User ID: \(uid)", for: .normal)
     }
 
     @IBAction func tapProfileImage(_ sender: Any) {
@@ -45,11 +51,27 @@ class ProfileVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             profileImage.image = image
-            
+            self.image = image
            // let userDb = UserDb()
-            
-            
         }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func userCodeButtonPress(_ sender: Any) {
+        let mf = MFMessageComposeViewController()
+        if !MFMessageComposeViewController.canSendText() {
+            print("SMS services are not available")
+            // add error message label
+            return
+        }
+        mf.messageComposeDelegate = self
+        mf.recipients = [] // Might have a way to autopopulate this
+        mf.body = "Add your friend \(self.user) on GhostRunner using the following code \(self.uid)"
+        
+        present(mf, animated: true, completion: nil)
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
         dismiss(animated: true, completion: nil)
     }
     
