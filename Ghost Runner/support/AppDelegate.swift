5cb95155eba329307c8c92104e89cc14cdca4bf2
Nikita Andrikanis <nandrikanis@ucdavis.edu>
2021-03-16 16:42:49 -0700

    Aaaand one more change

--- Ghost Runner/support/AppDelegate.swift
+++ Ghost Runner/support/AppDelegate.swift
@@ -13,11 +13,6 @@ import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    override init() {
-        super.init()
-        //UIFont.overrideInitialize()
-    }
 
     static let geoCoder = CLGeocoder()
     
@@ -54,78 +49,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
-struct SetCustomFont {
-    static let regular = "AvenirNext-DemiBold"
-    static let bold = "AvenirNext-Bold"
-    static let italic = "AvenirNext-DemiBoldItalic"
-}
-
-extension UIFontDescriptor.AttributeName {
-    static let nsctFontUIUsage = UIFontDescriptor.AttributeName(rawValue: "NSCTFontUIUsageAttribute")
-}
-
-extension UIFont {
-    static var isOverrided: Bool = false
-
-    @objc class func mySystemFont(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: SetCustomFont.regular, size: size)!
-    }
-
-    @objc class func myBoldSystemFont(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: SetCustomFont.bold, size: size)!
-    }
-
-    @objc class func myItalicSystemFont(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: SetCustomFont.italic, size: size)!
-    }
-
-    @objc convenience init(myCoder aDecoder: NSCoder) {
-        guard
-            let fontDescriptor = aDecoder.decodeObject(forKey: "UIFontDescriptor") as? UIFontDescriptor,
-            let fontAttribute = fontDescriptor.fontAttributes[.nsctFontUIUsage] as? String else {
-                self.init(myCoder: aDecoder)
-                return
-        }
-        var fontName = ""
-        switch fontAttribute {
-        case "CTFontRegularUsage":
-            fontName = SetCustomFont.regular
-        case "CTFontEmphasizedUsage", "CTFontBoldUsage":
-            fontName = SetCustomFont.bold
-        case "CTFontObliqueUsage":
-            fontName = SetCustomFont.italic
-        default:
-            fontName = SetCustomFont.regular
-        }
-        self.init(name: fontName, size: fontDescriptor.pointSize)!
-    }
-
-    class func overrideInitialize() {
-        guard self == UIFont.self, !isOverrided else { return }
-
-        // Avoid method swizzling run twice and revert to original initialize function
-        isOverrided = true
-
-        if let systemFontMethod = class_getClassMethod(self, #selector(systemFont(ofSize:))),
-            let mySystemFontMethod = class_getClassMethod(self, #selector(mySystemFont(ofSize:))) {
-            method_exchangeImplementations(systemFontMethod, mySystemFontMethod)
-        }
-
-        if let boldSystemFontMethod = class_getClassMethod(self, #selector(boldSystemFont(ofSize:))),
-            let myBoldSystemFontMethod = class_getClassMethod(self, #selector(myBoldSystemFont(ofSize:))) {
-            method_exchangeImplementations(boldSystemFontMethod, myBoldSystemFontMethod)
-        }
-
-        if let italicSystemFontMethod = class_getClassMethod(self, #selector(italicSystemFont(ofSize:))),
-            let myItalicSystemFontMethod = class_getClassMethod(self, #selector(myItalicSystemFont(ofSize:))) {
-            method_exchangeImplementations(italicSystemFontMethod, myItalicSystemFontMethod)
-        }
-
-        if let initCoderMethod = class_getInstanceMethod(self, #selector(UIFontDescriptor.init(coder:))), // Trick to get over the lack of UIFont.init(coder:))
-            let myInitCoderMethod = class_getInstanceMethod(self, #selector(UIFont.init(myCoder:))) {
-            method_exchangeImplementations(initCoderMethod, myInitCoderMethod)
-        }
-    }
-}
-
